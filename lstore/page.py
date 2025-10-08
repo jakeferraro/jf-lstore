793ca3a0c9a2dd1ffdb8c1c87848971c60bd04d3
Jake Ferraro <jakeferraro@proton.me>
2025-10-08 14:43:38 -0700

    implement page.py

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,15 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        return self.num_records < 512
 
     def write(self, value):
+        position = self.num_records * 8
+        byte_val = value.to_bytes(8, byteorder='big')
+        self.data[position:position+8] = byte_val
         self.num_records += 1
-        pass
 
+    def read(self, index):
+        position = index * 8
+        data_bytes = self.data[position:position+8]
+        return int.from_bytes(data_bytes, byteorder='big') 
